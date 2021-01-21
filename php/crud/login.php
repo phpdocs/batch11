5bc0735699c7220de92fd444006ab82891130ccf
Mac <mac@Afzal-Mac-Book-Pro-6.local>
2021-01-21 10:49:49 +0500

    Login/Logout

--- php/crud/login.php
+++ php/crud/login.php
@@ -1,36 +1,41 @@
-<?php require_once("template/header.php"); ?>
+<?php 
+    require_once("template/header.php");
+    require_once("helper/login_helper.php");
+ ?>
     <div class="container-fluid">
-
         <div class="row justify-content-center">
             <div class="col-12 col-sm-12 col-md-8 col-lg-4 col-xl-4">
                 <div class="card " style="width: 80%">
                     <img class="card-img-top" style="width:99%" src="https://icon-library.com/images/google-login-icon/google-login-icon-0.jpg" alt="Card image cap">
                     <div class="card-body">
                     <h5 class="card-title">Login</h5>
+                    <p><?php echo $LoginMessage; ?>
+                   <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
                     <div class="row justify-content-center mt-3">
-                        <div class="col-6 text-right mt-auto">
-                            <h5>UserName</h5>
+                            <div class="col-6 text-right mt-auto">
+                                <h5>UserName</h5>
+                            </div>
+                            <div class="col-6 text-left">
+                                <input type="text" name="txtUserName" required class="form-control" />
+                            </div>
                         </div>
-                        <div class="col-6 text-left">
-                            <input type="text" name="txtUserName" required class="form-control" />
+                        <div class="row justify-content-center mt-3">
+                            <div class="col-6 text-right mt-auto">
+                                <h5>Password</h5>
+                            </div>
+                            <div class="col-6 text-left">
+                                <input type="password" name="txtPassword" required class="form-control" />
+                            </div>
                         </div>
-                    </div>
-                    <div class="row justify-content-center mt-3">
-                        <div class="col-6 text-right mt-auto">
-                            <h5>Password</h5>
+                        <div class="row justify-content-center mt-3">
+                            <div class="col-6 text-right mt-auto">
+                                <input type="Reset" value="Clear" class="btn btn-danger" />
+                            </div>
+                            <div class="col-6 text-left">
+                            <input type="submit" name="btnLogin" value="Login" class="btn btn-primary" />
+                            </div>
                         </div>
-                        <div class="col-6 text-left">
-                            <input type="password" name="txtPassword" required class="form-control" />
-                        </div>
-                    </div>
-                    <div class="row justify-content-center mt-3">
-                        <div class="col-6 text-right mt-auto">
-                            <input type="Reset" value="Clear" class="btn btn-danger" />
-                        </div>
-                        <div class="col-6 text-left">
-                        <input type="submit" value="Login" class="btn btn-primary" />
-                        </div>
-                    </div>
+                   </form>
                 </div>
             </div>
             </div>
---
+++ php/crud/product.php
@@ -0,0 +1,15 @@
+<?php require_once("helper/auth_helper.php"); ?>
+<?php require_once("template/header.php"); ?>
+
+    <!--Container Area Start-->
+    <div class="container-fluid">
+        <div class="row my-2">
+            <div class="col-12 text-center">
+                <h2>Product Management</h2>
+            </div>
+        </div>
+    </div>
+    <!--Create Grid for Users End
+    </div>
+    <!--Container Area End-->
+<?php require_once("template/footer.php"); ?>
